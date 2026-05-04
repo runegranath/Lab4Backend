bceb101ac7a09a630ce35fcbf93da4414efc0c48
runegranath <rune.granath@gmail.com>
2026-05-04 17:02:33 +0200

    säker route tillagd med JWT

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,9 +3,11 @@ const express = require("express");
 const router = express.Router();
 const sqlite3 = require("sqlite3").verbose();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Anslut till db
 const db = new sqlite3.Database(process.env.DATABASE);
+
 // Lägg till användare
 router.post("/register", async (req, res) => {
   try {
@@ -74,8 +76,16 @@ router.post("/login", async (req, res) => {
         if (!passwordMatch) {
           res.status(401).json({ message: "Felaktigt användarnamn/lösenord!" });
         } else {
+          // Skapa JWT
+          const payload = { username: username };
+          const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+          const response = {
+            message: "Användare inloggad!",
+            token: token
+          }
+
           //Korrekt login
-          res.status(200).json({ message: "korrekt inloggning!" });
+          res.status(200).json({ response });
         }
       }
     });
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-process.env.PORT
+process.env.PORT;
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,7 +13,31 @@ app.use(bodyParser.json());
 // Routes
 app.use("/api", authRoutes);
 
+// Skyddade routes
+app.get("api/protected", authenticateToken, (req, res) => {
+  res.json({ message: "Skyddad route!" });
+  // Hamna här vid next och får skyddad info
+});
+
+// Validera Token
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Token
+
+  if (token == null)
+    res
+      .status(401)
+      .json({ message: "Ej auktoriserad för denna route - token saknas! " });
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, username) => {
+    if (err) return res.status(403).json({ message: "Ej korrekt JWT" });
+
+    req.username = username;
+    next();
+  });
+}
+
 // Start app
 app.listen(port, () => {
-    console.log(`Server igång på http://localhost:${port}`);
-})
+  console.log(`Server igång på http://localhost:${port}`);
+});
